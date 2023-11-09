47646de18b53aebb0aa3e6e3e9c33a895b4778db
7max18 <maxkoppel0703@gmail.com>
2023-11-09 18:00:55 -0500

    Added file parse and dictionary search

--- puflantu.py
+++ puflantu.py
@@ -1,4 +1,4 @@
-import csv, argparse, re
+import csv, argparse, re, sys
 from itertools import chain, combinations
 from modifiers import prefixes, suffixes, pronouns, all_pronouns, pronoun_meanings
 import colorama
@@ -22,12 +22,37 @@ def get_dictionary():
         dictionary = { rows['puflantu']:rows['english'] for rows in reader }
     return dictionary
 
+def clean_text(sentence):
+    sentence = sentence.lower()
+    p = re.compile(' ')
+    if p.search(sentence):
+        sentence = sentence.split()
+    else:
+        sentence = [sentence]
+    parsed_sentence = list()
+    for word in sentence:
+        word = ''.join([x for x in word if x in '\'abcdefghijklmnopqrstuvwxyz-'])
+        if word:
+            parsed_sentence.append(word)
+    sentence = parsed_sentence
+    return sentence
+
+def print_translated_line(sentence, translation):
+    for i, v in enumerate(sentence):
+        spacing = max(len(v), len(translation[i]))
+        print(Fore.LIGHTBLUE_EX+f'{v:{spacing}}', end='  ')
+    print(Style.RESET_ALL)
+    for i, v in enumerate(translation):
+        spacing = max(len(v), len(sentence[i]))
+        print(f'{v:{spacing}}', end='  ')
+    print()
+
 def translate(sentence):
     dictionary = get_dictionary()
     full_translation = list()
-    combined_prefixes = "((" + ")|(".join(prefixes.keys()) + "))*"
-    combined_suffixes = "((" + ")|(".join(suffixes.keys()) + "))*"
-    combined_pronouns = "((" + ")|(".join(all_pronouns) + "))*"
+    combined_prefixes = '((' + ')|('.join(prefixes.keys()) + '))*'
+    combined_suffixes = '((' + ')|('.join(suffixes.keys()) + '))*'
+    combined_pronouns = '((' + ')|('.join(all_pronouns) + '))*'
     regex_prefixes = [(pattern, re.compile(r'^'+combined_prefixes+pattern+r'\w+')) for pattern in prefixes.keys()]
     regex_suffixes = [(pattern, re.compile(r'\w+'+pattern+combined_suffixes+r'$')) for pattern in suffixes.keys()]
     regex_pronouns = [(pattern, re.compile(r'\w+'+combined_pronouns+r'?'+pattern+combined_pronouns+r'?[aeiouw]([^aeiouw]){1,2}')) for pattern in all_pronouns]
@@ -67,30 +92,29 @@ def translate(sentence):
         if light_match:
             all_candidates.add(light_match.group()[0])
         if fluid_match:
-            if "ur" in fluid_match.group():
+            if 'ur' in fluid_match.group():
                 all_candidates.add(fluid_match.group()[-3:-1])
             else:
                 all_candidates.add(fluid_match.group()[-2:-1])
         if physical_match:
-            all_candidates.add(physical_match.group()[:physical_match.group().find("w")+1])
+            all_candidates.add(physical_match.group()[:physical_match.group().find('w')+1])
         if n_negation_match:
-            all_candidates.add("ay")
+            all_candidates.add('ay')
         if v_negation_match:
-            all_candidates.add("ey")
+            all_candidates.add('ey')
         potential_roots = list()
         for candidate in chain.from_iterable(combinations(all_candidates, r) for r in range(len(all_candidates)+1)):
             root = word
             for substr in candidate:
                 root = root.replace(substr, '')
-            print(root)
             if root in dictionary:
                 potential_roots.append((root, candidate))
         if potential_roots:
             root, candidate = min(potential_roots, key=lambda x:len(x[0]))
             for prefix in present_prefixes:
                 if prefix in candidate:
-                    if prefix == "\'":
-                        post_translated += "!"
+                    if prefix == '\'':
+                        post_translated += '!'
                     else:
                         pre_translated += prefixes[prefix] + '-'
             for suffix in present_suffixes:
@@ -109,77 +133,78 @@ def translate(sentence):
                                 post_translated += '-' + pronoun_meanings[path[0]][path[1]]
             if light_match:
                 if light_match.group()[0] in candidate:
-                    post_translated += "&light-source"
+                    post_translated += '&light-source'
             if fluid_match:
                 if fluid_match.group()[-3:-1] in candidate:
-                    post_translated += "&fluid"
+                    post_translated += '&fluid'
             if physical_match:
-                if physical_match.group()[:physical_match.group().find("w")+1] in candidate:
-                    post_translated += "&bodily-action"
+                if physical_match.group()[:physical_match.group().find('w')+1] in candidate:
+                    post_translated += '&bodily-action'
             if n_negation_match:
-                if "ay" in candidate:
-                    pre_translated += "not-"
+                if 'ay' in candidate:
+                    pre_translated += 'not-'
             if v_negation_match:
-                if "ey" in candidate:
-                    pre_translated += "not-"
+                if 'ey' in candidate:
+                    pre_translated += 'not-'
             word = root
         #check for dual/pluralization
-        if word[-1] == "w":
+        if word[-1] == 'w':
             p = re.compile(word[:-1]+r'[aeiouw]')
             for check in dictionary.keys():
                 if p.match(check):
                     word = check
-                    post_translated = "-dual" + post_translated
+                    post_translated = '-dual' + post_translated
                     break
-        elif word[-2:] == "we":
+        elif word[-2:] == 'we':
             p = re.compile(word[:-2]+r'[aeiouw]')
             for check in dictionary.keys():
                 if p.match(check):
                     word = check
-                    if dictionary[check][-1] != "s":
-                        post_translated = "s" + post_translated
+                    if dictionary[check][-1] != 's':
+                        post_translated = 's' + post_translated
                     else:
-                        post_translated = "es" + post_translated
+                        post_translated = 'es' + post_translated
                     break
         if word in dictionary:
             full_translation.append(pre_translated+dictionary[word]+post_translated+enclitic_translated)
         else:
-            full_translation.append(pre_translated+"UNKNOWN"+post_translated+enclitic_translated)
+            full_translation.append(pre_translated+'UNKNOWN'+post_translated+enclitic_translated)
     return full_translation
 
 parser = argparse.ArgumentParser(
     prog='puflantu',
     description='Puflantu translation utility'
 )
-
-parser.add_argument("text", help="text to be translated from puflantu")
-parser.add_argument('-N', '-natural', help='Translate into proper English', dest='natural', action='store_true')
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-t', '--text', nargs='?', help='text to translate from Puflantu')
+#parser.add_argument('-N', '-natural', help='Translate into proper English', dest='natural', action='store_true')
+group.add_argument('-f', '--file', nargs='?', help='translate a file from Puflantu', type=argparse.FileType('r'))
+group.add_argument('-s', '--search', nargs='?', help='search for an English word\'s Puflantu translation')
 
 if __name__ == '__main__':
     args = parser.parse_args()
     if args.text:
-        sentence = args.text.lower()
-        p = re.compile(" ")
-        if p.search(sentence):
-            sentence = sentence.split()
-        else:
-            sentence = [sentence]
-        parsed_sentence = list()
-        for word in sentence:
-            word = ''.join([x for x in word if x in "\'abcdefghijklmnopqrstuvwxyz-"])
-            if word:
-                parsed_sentence.append(word)
-        sentence = parsed_sentence
+        sentence = args.text
+        sentence = clean_text(sentence)
         translation = translate(sentence)
-        if args.natural:
-            pass
+        print_translated_line(sentence, translation)
+    elif args.file:
+        for sentence in args.file.readlines():
+            sentence = clean_text(sentence)
+            translation = translate(sentence)
+            print_translated_line(sentence, translation)
+            print()
+    elif args.search:
+        query = args.search.lower()
+        dictionary = get_dictionary()
+        for key, value in dictionary.items():
+            if value.lower() == query:
+                print(f"The Puflantu word for \"{query}\" is \"{key}.\"")
+                break
         else:
-            for i, v in enumerate(sentence):
-                spacing = max(len(v), len(translation[i]))
-                print(Fore.LIGHTBLUE_EX+f"{v:{spacing}}", end="  ")
-            print(Style.RESET_ALL)
-            for i, v in enumerate(translation):
-                spacing = max(len(v), len(sentence[i]))
-                print(f"{v:{spacing}}", end="  ")
+            print(f"There is no known Puflantu word for \"{query}.\"")
+
+
+            
             
 
